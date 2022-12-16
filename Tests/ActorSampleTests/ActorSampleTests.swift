00d013be4a95e1ccd13568c0277022367349d4a4
Mikhail Rubanov <m.rubanov@dodopizza.com>
2022-12-16 16:17:27 +0500

    Clean code

--- Tests/ActorSampleTests/ActorSampleTests.swift
+++ Tests/ActorSampleTests/ActorSampleTests.swift
@@ -6,50 +6,25 @@ final class ActorSampleTests: XCTestCase {
     func testPrefetchAndGet() {
         let sut = PaymentMethodService()
         
-        for i in 0..<1000 {
+        for i in 0..<10000 {
             DispatchQueue.global(qos: .userInitiated).async {
-                sut.prefetch(unitId: "1")
+                sut.prefetch(unitId: i)
             }
             DispatchQueue.main.async {
                 _ = sut.paymentMethods
             }
         }
     }
-    
-    func testPrefetchAndGet_Actor() async {
-        let sut = PaymentMethodActor()
-        
-        await sut.prefetch(unitId: "1")
-        
-        let methods = await sut.paymentMethods
-        XCTAssertEqual(methods,
-                       [PaymentMethod(name: "ApplePay"),
-                        PaymentMethod(name: "SberPay")])
-    }
 }
 
 class PaymentMethodService {
-    func prefetch(unitId: String) {
-        self.paymentMethods = [PaymentMethod(name: "ApplePay"),
-                               PaymentMethod(name: "SberPay")]
+    func prefetch(unitId: Int) {
+        self.paymentMethods = [PaymentMethod(id: unitId)]
     }
     
-    var paymentMethods: [PaymentMethod] = []
-}
-
-// 1. Allow synchronous access to actor’s members from within itself,
-// 2. Allow only asynchronous access to the actor’s members from any asynchronous context, and
-// 3. Allow only asynchronous access to the actor’s members from outside the actor.
-actor PaymentMethodActor {
-    func prefetch(unitId: String) {
-        self.paymentMethods = [PaymentMethod(name: "ApplePay"),
-                               PaymentMethod(name: "SberPay")]
-        
-    }
-
     var paymentMethods: [PaymentMethod] = []
 }
 
 struct PaymentMethod: Equatable {
-    let name: String
+    let id: Int
 }
