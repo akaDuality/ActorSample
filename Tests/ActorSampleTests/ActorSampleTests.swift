adf8ccaddf18d0cd1a6795cc31e669a6877be6b5
Mikhail Rubanov <m.rubanov@dodopizza.com>
2022-12-16 15:55:31 +0500

    Add failing test

--- Tests/ActorSampleTests/ActorSampleTests.swift
+++ Tests/ActorSampleTests/ActorSampleTests.swift
@@ -2,10 +2,42 @@ import XCTest
 @testable import ActorSample
 
 final class ActorSampleTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ActorSample().text, "Hello, World!")
+
+    func testPrefetchAndGet() {
+        let sut = PaymentMethodService()
+        
+        sut.prefetch(unitId: "1")
+        
+        XCTAssertEqual(sut.paymentMethods,
+                       [PaymentMethod(name: "ApplePay"),
+                        PaymentMethod(name: "SberPay")])
     }
+}
+
+class PaymentMethodService {
+    func prefetch(unitId: String) {
+        DispatchQueue.main.async {
+            self.paymentMethods = [PaymentMethod(name: "ApplePay"),
+                                   PaymentMethod(name: "SberPay")]
+        }
+    }
+    
+    var paymentMethods: [PaymentMethod] = []
+}
+
+
+//actor PaymentMethodActor {
+//    func prefetch(unitId: String) {
+//        DispatchQueue.main.async {
+//            self.paymentMethods = [PaymentMethod(name: "ApplePay"),
+//                                   PaymentMethod(name: "SberPay")]
+//        }
+//
+//    }
+//
+//    var paymentMethods: [PaymentMethod] = []
+//}
+
+struct PaymentMethod: Equatable {
+    let name: String
 }
