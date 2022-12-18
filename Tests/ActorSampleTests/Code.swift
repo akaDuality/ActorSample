aa6ad6d28c78e395dfde560909367d983d2b8efd
Mikhail Rubanov <m.rubanov@dodopizza.com>
2022-12-18 09:14:36 +0500

    Add sample code

---
+++ Tests/ActorSampleTests/Code.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+struct PaymentMethod: Equatable {
+    let id: Int
+}
+
+class PaymentMethodService_Background {
+    
+    let concurrentQueue = DispatchQueue.global(qos: .background)
+    
+    func prefetch(unitId: Int) {
+        concurrentQueue.async {
+            self.paymentMethods = [PaymentMethod(id: unitId)]
+        }
+    }
+
+    var paymentMethods: [PaymentMethod] = []
+}
+
+class PaymentMethodService_Barier {
+    
+    let serialQueue = DispatchQueue(label: "Payment")
+    
+    func prefetch(unitId: Int) {
+        serialQueue.async {
+            self._paymentMethods = [PaymentMethod(id: unitId)]
+        }
+    }
+    
+    
+    private var _paymentMethods: [PaymentMethod] = []
+    var paymentMethods: [PaymentMethod] {
+        serialQueue.sync(flags: .barrier, execute: {
+            return _paymentMethods
+        })
+    }
+}
+
+// https://stackoverflow.com/questions/58236153/dispatchqueue-sync-vs-sync-barrier-in-concurrent-queue
+
+
+
+
+/// https://trycombine.com/posts/swift-actors/
+/// 1. Allow synchronous access to actor’s members from within itself,
+/// 2. Allow only asynchronous access to the actor’s members from any asynchronous context, and
+/// 3. Allow only asynchronous access to the actor’s members from outside the actor.
+///
+/// This way the actor itself accesses its data synchronously,
+/// but any other code from outside is required asynchronous access
+/// (with implicit synchronization) to prevent data races.
+
+actor PaymentMethodActor {
+    func prefetch(unitId: Int) {
+        self.paymentMethods = [PaymentMethod(id: unitId)]
+    }
+    
+    var paymentMethods: [PaymentMethod] = []
+}
